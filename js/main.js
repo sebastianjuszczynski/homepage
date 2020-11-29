4c9addee9da4a1c22d6b243dd5a03b6e49f10a7c
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-11-29 23:42:23 +0000

    refactoring js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,70 @@
-console.log("Siemanko!!!✋");
-
-let container = document.querySelector(".container");
-let toggleTheme = document.querySelector(".button--js-themeChange");
-let toggleSize = document.querySelector(".button--js-imageSizeChange");
-let image = document.querySelector(".image");
-let tableDatas = [...document.querySelectorAll(".table__data")];
-let buttons = [...document.querySelectorAll(".buttons")];
-let navigationLinks = [...document.querySelectorAll(".navigation__link")];
-
-
-
-toggleTheme.addEventListener("click", () => {
-    container.classList.toggle("container--js-darkTheme");
-    tableDatas.forEach(tableData => tableData.classList.toggle("table__data--js-darkTheme"));
-    buttons.forEach(button => button.classList.toggle("buttons--js-darkTheme"));
-    navigationLinks.forEach(navigationLink => navigationLink.classList.toggle("navigation__link--js-darkTheme"));
-    if (toggleTheme.innerText === "Zmień na ciemny motyw") {
-        toggleTheme.innerText = "Zmień na jasny motyw"
+{
+    const welcomeMessage = () => {
+        console.log("Siemanko!!!✋");
     }
-    else if (toggleTheme.innerText === "Zmień na jasny motyw") {
-        toggleTheme.innerText = "Zmień na ciemny motyw"
-    }
-});
-toggleSize.addEventListener("click", () => {
-    image.classList.toggle("image--js-sizeChange")
-});
+    welcomeMessage();
 
+    const navigationLinksColorBackgroundChange = () => {
+        let navigationLinks = [...document.querySelectorAll(".navigation__link")];
+        navigationLinks.forEach(navigationLink => navigationLink.classList.toggle("navigation__link--js-darkTheme"));
+
+    }
+
+    const buttonsBackgroundColorChange = () => {
+        let buttons = [...document.querySelectorAll(".buttons")];
+        buttons.forEach(button => button.classList.toggle("buttons--js-darkTheme"));
+
+    }
+
+    const toggleBackgroundColorTable = () => {
+        let tableDatas = [...document.querySelectorAll(".table__data")];
+        tableDatas.forEach(tableData => tableData.classList.toggle("table__data--js-darkTheme"));
+
+    }
+
+    const changeBackgroundColor = () => {
+        let container = document.querySelector(".container");
+        container.classList.toggle("container--js-darkTheme");
+
+    }
+
+    const buttonsTextChange = () => {
+        let toggleTheme = document.querySelector(".button--js-themeChange");
+
+        if (toggleTheme.innerText === "Zmień na ciemny motyw") {
+            toggleTheme.innerText = "Zmień na jasny motyw"
+        }
+        else if (toggleTheme.innerText === "Zmień na jasny motyw") {
+            toggleTheme.innerText = "Zmień na ciemny motyw"
+        }
+    }
+    const changeImageSize = () => {
+        let image = document.querySelector(".image");
+        let toggleSize = document.querySelector(".button--js-imageSizeChange");
+        toggleSize.addEventListener("click", () => {
+            image.classList.toggle("image--js-sizeChange")
+        });
+    }
+
+
+    const init = () => {
+        let toggleTheme = document.querySelector(".button--js-themeChange");
+        toggleTheme.addEventListener("click", () => {
+            let container = document.querySelector(".container");
+            container.classList.toggle("container--js-darkTheme");
+            buttonsTextChange();
+            toggleBackgroundColorTable();
+            buttonsBackgroundColorChange();
+            navigationLinksColorBackgroundChange();
+            changeImageSize();
+        });
+
+
+
+    }
+    init();
+
+}
 
 
 
